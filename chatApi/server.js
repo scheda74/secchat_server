64da551c29ce60890d4f30a21c7fed59d01c01c9
David Schemm <davidschemm95@gmail.com>
2018-09-20 18:14:45 -0700

    wrong changes

--- chatApi/server.js
+++ chatApi/server.js
@@ -7,7 +7,7 @@ var express = require('express'),
   
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://https://www.secchat.me/chatDb'); 
+mongoose.connect('mongodb://www.secchat.me/chatDb'); 
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
