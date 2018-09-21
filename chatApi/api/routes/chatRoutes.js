7f66a11d449091354dc9f201eee6285b01b4a697
David Schemm <davidschemm95@gmail.com>
2018-09-20 18:35:07 -0700

    route changes

--- chatApi/api/routes/chatRoutes.js
+++ chatApi/api/routes/chatRoutes.js
@@ -3,12 +3,12 @@ module.exports = function(app) {
   var chat = require('../controllers/chatController');
 
   // todoList Routes
-  app.route('/usr/messages')
+  app.route('/chats/messages')
     .get(chat.list_all_messages, (err) => {})
     .post(chat.send_a_message, (err) => {});
 
 
-  app.route('/usr/:chatId')
+  app.route('/chats/:chatId')
     .get(chat.get_a_chat, (err) => {})
     // .put(chat.update_a_message) // is this even possible?
     //.delete(chat.delete_a_message, (err) => {}); // only in DB not at client - this can be implemented tho
