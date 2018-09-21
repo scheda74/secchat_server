f7de95f805a285ac397ad477d5cf0497b760b664
David Schemm <davidschemm95@gmail.com>
2018-09-20 17:56:56 -0700

    node server updated

--- chatApi/api/routes/chatRoutes.js
+++ chatApi/api/routes/chatRoutes.js
@@ -11,5 +11,5 @@ module.exports = function(app) {
   app.route('/usr/:chatId')
     .get(chat.get_a_chat, (err) => {})
     // .put(chat.update_a_message) // is this even possible?
-    .delete(chat.delete_a_message, (err) => {}); // only in DB not at client - this can be implemented tho
+    //.delete(chat.delete_a_message, (err) => {}); // only in DB not at client - this can be implemented tho
 };
