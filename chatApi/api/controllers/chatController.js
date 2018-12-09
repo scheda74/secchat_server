53c9fe5e947790ba009071b251b95bc5e1a98804
David Schemm <davidschemm95@gmail.com>
2018-12-08 18:53:49 -0800

    s

--- chatApi/api/controllers/chatController.js
+++ chatApi/api/controllers/chatController.js
@@ -41,7 +41,7 @@ exports.send_a_message = function(req, res) {
         if(!usr) {
             return res.status(403).send({ success: false, message: 'User authentication failed'});
         } else {
-            console.log('data ' + req.body.data);
+            req.body.data.json().then((dat) => console.log(dat));
             var new_msg = new Chat({ user: [usr], receiver: req.body.receiver, data: [req.body.data] });
             console.log(new_msg)
             new_msg.save(function(err, msg) {
